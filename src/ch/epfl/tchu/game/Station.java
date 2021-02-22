bfa2c5c4c5a7349ea968a0c1e51b147922121523
Shangeeth2001 <shan2001@hotmail.ch>
2021-02-22 21:47:19 +0100

    22.02.2021 21:46 @Shangeeth

--- src/ch/epfl/tchu/game/Station.java
+++ src/ch/epfl/tchu/game/Station.java
@@ -1,4 +1,7 @@
 package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+
 /**
  * Class Station
  *
@@ -10,9 +13,11 @@ public class Station {
     private final int id; // entre 0 et 50
 
     public Station(int id, String name){
+        /*
         if(id < 0 || id >50){ // je sais pas si le > 50 je dois le mettre
             throw new IllegalArgumentException();
-        }
+        }*/
+        Preconditions.checkArgument(id >= 0 && id <=50);
         this.name = name;
         this.id = id;
     }
--- src/ch/epfl/tchu/game/StationConnectivity.java
+++ src/ch/epfl/tchu/game/StationConnectivity.java
@@ -1,4 +1,11 @@
 package ch.epfl.tchu.game;
-
+/**
+ * Interface StationConnectivity
+ *
+ * @author Shangeeth Poobalasingam (329307)
+ * @author Marvin Koch (324448)
+ */
 public interface StationConnectivity {
+    boolean connected(Station s1, Station s2);
+
 }
