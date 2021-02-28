12567b68dc17cb9cc01e735e80b1aea52ecce604
marvin <marvin@koch-family.ch>
2021-02-28 18:04:29 +0100

    28/02/21 18:00 @Marvin

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -12,14 +12,13 @@ import java.util.TreeSet;
  * @author Shangeeth Poobalasingam (329307)
  * @author Marvin Koch (324448)
  */
-
 public final class Ticket implements Comparable<Ticket>{
     private final List<Trip> trips;
     private final String text;
 
     /**
      * Contructeur primaire de Ticket
-     * @param trips liste de trips du billet
+     * @param trips liste de trips du ticket
      * @throws IllegalArgumentException si la liste de trips est vide
      */
     public Ticket(List<Trip> trips){
@@ -37,14 +36,20 @@ public final class Ticket implements Comparable<Ticket>{
 
     /**
      * Constructeur secondaire de Ticket, utilisé s'il contient qu'un trajet
-     * @param from station de départ trajet
-     * @param to station d'arrivée trajet
+     * @param from station de départ du trajet
+     * @param to station d'arrivée du trajet
      * @param points nombre de points du trajet
      */
     public Ticket(Station from, Station to, int points){
         this(List.of(new Trip(from, to, points)));
     }
 
+    /**
+     * Méthode qui compare 2 tickets
+     * @param that le ticket auquel il compare
+     * @return retourne un entier négatif quelconque si la première vient avant la seconde dans l'ordre alphabétique,
+     * zéro si les deux sont égales, et un entier positif quelconque sinon.
+     */
     @Override
     public int compareTo(Ticket that) {
         return this
@@ -61,9 +66,9 @@ public final class Ticket implements Comparable<Ticket>{
     }
 
     /**
-     * Calcule et la représentation textuelle du billet
+     * Calcule et la représentation textuelle du ticket
      * @param trips liste des trajets
-     * @return le string qui représentent le billet
+     * @return le string qui représentent le ticket
      */
     private static String computeText(List<Trip> trips){
         if(trips.size() == 1){
@@ -79,7 +84,7 @@ public final class Ticket implements Comparable<Ticket>{
     }
 
     /**
-     * Calcule et retourne le nombre de points du billet
+     * Calcule et retourne le nombre de points du ticket
      * @param connectivity
      * @return le nombre de points
      */
