99017ba442e1fe4d0c5994cf33c07b45576c9379
Shangeeth2001 <shan2001@hotmail.ch>
2021-03-10 10:37:49 +0100

    10.03.2021 10:37 @Shangeeth

--- test/ch/epfl/tchu/game/PublicCardStateTest.java
+++ test/ch/epfl/tchu/game/PublicCardStateTest.java
@@ -1,20 +1,36 @@
 package ch.epfl.tchu.game;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PublicCardStateTest {
     @Test
-    void testConstructeur(){
+    void testConstructeurAndOther(){
         List<Card> cards5= new ArrayList<>();
         cards5.add(Card.BLACK);
         cards5.add(Card.BLACK);
         cards5.add(Card.LOCOMOTIVE);
         cards5.add(Card.BLACK);
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PublicCardState(cards5,5,6);
+        });
         cards5.add(Card.BLUE);
-
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PublicCardState(cards5,-5,6);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PublicCardState(cards5,5,-6);
+        });
+        PublicCardState cardState =  new PublicCardState(cards5,0,6);
+        assertEquals(11,cardState.totalSize());
+        assertEquals(Card.LOCOMOTIVE,cardState.faceUpCard(2));
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            cardState.faceUpCard(5);
+        });
+        assertEquals(true,cardState.isDeckEmpty());
 
     }
 
